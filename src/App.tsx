ddeb87fce09b981125fb5b119a318beae36d83bb
Xu <mhoguty0213@gmail.com>
2023-03-06 11:50:48 +0700

    Hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from './layouts';
-import { Fragment } from 'react';
 
 function App() {
     return (
@@ -17,7 +16,7 @@ function App() {
                         if (route.layout) {
                             Layout = route.layout;
                         } else if (route.layout === null) {
-                            Layout = Fragment;
+                            Layout = React.Fragment;
                         }
 
                         return (
