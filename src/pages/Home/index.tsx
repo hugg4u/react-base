0668f5a60f0eae0e9b1cce66cff5c3c443ac6f16
hungdao <hung2k3vn@gmail.com>
2024-05-07 22:16:11 +0700

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Home() {
-    return <div>Home</div>;
+    return <div>Home
+        <div>
+            <p>This is home page</p>
+        </div>
+    </div>;
+
 }
